eb2b4e5c1502ea80d7a0563145260f214b70ee74
wends05 <cassandraysabel.gallo-21@cpu.edu.ph>
2024-09-02 22:05:57 +0800

    first commit

---
+++ src/main.ts
@@ -0,0 +1,32 @@
+const inputBox = document.getElementById('enter-task') as HTMLInputElement;
+const listContainer = document.getElementById('list-container');
+
+function addTask() {
+
+    if (!inputBox || !listContainer) return;
+
+    if (inputBox.value === '') {
+        alert('Please enter a task');
+    } else {
+        let li = document.createElement("li");
+        li.textContent = inputBox.value; 
+        listContainer.appendChild(li);
+
+        let span = document.createElement("span");
+        span.textContent = "x";  
+        li.appendChild(span);
+    }
+    inputBox.value = '';  
+}
+
+
+
+listContainer?.addEventListener("click", function(e) {
+    const target = e.target as HTMLElement;
+
+    if (target.tagName === "LI") {
+        target.classList.toggle("done");
+    } else if (target.tagName === "SPAN") {
+        target.parentElement?.remove();
+    }
+}, false);
